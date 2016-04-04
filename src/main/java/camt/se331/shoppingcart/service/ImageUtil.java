538f4f718a31c29aa28887f2436cc665f986a175
PJQuEz <peerapat_j@cmu.ac.th>
2016-04-05 01:32:22 +0700

    update to 3.15

--- src/main/java/camt/se331/shoppingcart/service/ImageUtil.java
+++ src/main/java/camt/se331/shoppingcart/service/ImageUtil.java
@@ -1,7 +1,10 @@
 package camt.se331.shoppingcart.service;
 
 import camt.se331.shoppingcart.entity.Image;
+import org.imgscalr.Scalr;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,18 +18,21 @@ import java.util.Calendar;
 public class ImageUtil {
     static ImageUtil imageUtil = null;
 
-    public static ImageUtil getInstance() {
-        if (imageUtil == null) {
+    public static ImageUtil getInstance(){
+        if (imageUtil == null){
             imageUtil = new ImageUtil();
         }
         return imageUtil;
     }
 
-    public static Image getImage(String resourcePath) {
+    public static Image getImage(String resourcePath) throws IOException {
         Image image = new Image();
         ClassLoader classLoader = ImageUtil.getInstance().getClass().getClassLoader();
-
         File file = new File(classLoader.getResource(resourcePath).getFile());
+
+        BufferedImage originalImage= ImageIO.read(file);
+        BufferedImage ImageResize = Scalr.resize(originalImage,Scalr.Method.QUALITY,
+                200, 200);
 
         try {
 
@@ -34,9 +40,12 @@ public class ImageUtil {
             image.setContentType(Files.probeContentType(file.toPath()));
             FileInputStream fis = new FileInputStream(file);
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            ImageIO.write(ImageResize,"jpg",bos);
+
             byte[] buf = new byte[1024];
-            for (int readNum; (readNum = fis.read(buf)) != -1; ) {
-                bos.write(buf, 0, readNum);
+            for (int readNum; (readNum = fis.read(buf)) != -1;){
+                bos.write(buf,0,readNum);
             }
             image.setContent(bos.toByteArray());
             image.setCreated(Calendar.getInstance().getTime());
@@ -46,5 +55,4 @@ public class ImageUtil {
         return image;
 
     }
-
 }
